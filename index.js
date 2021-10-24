a0a7a9e3471a008a8e92d0f4aa361db797681dcb
Nutan Pundlik Bhoyar <70832372+nutan04@users.noreply.github.com>
2021-10-24 12:54:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+const replaceVal=(tempVal,orgVal)=>{
+let temperature =tempVal.replace("{%tempval%}",orgVal.main.temp);
+temperature =temperature.replace("{%tempmin%}",orgVal.main.temp_min);
+temperature =temperature.replace("{%tempmax%}",orgVal.main.temp_max);
+temperature =temperature.replace("{%location%}",orgVal.name);
+temperature =temperature.replace("{%country%}",orgVal.sys.country);
+
+return temperature
+};
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      "https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=fac1c4c9b7b9265a442d60af3efcc9b4",
+       )
+      .on('data',  (chunk) =>{
+        const objdata=JSON.parse(chunk);
+        const arrData =[objdata];
+       // console.log(arrData[0].main.temp);
+       const realTimeData =arrData.map(val=>replaceVal(homeFile,val)).join("")
+        res.write(realTimeData)
+       //console.log(realTimeData)
+      })
+      .on('end',  (err)=> {
+        if (err) return console.log('connection closed due to errors', err);
+
+        res.end();
+      });
+  }
+});
+
+server.listen(8009,"127.0.0.1")
